3e1eb5a44102bc33710c33f5d11acffa55e49f02
Vikas Kumar <vikask78966@gmail.com>
2026-09-20 23:05:53 +0530

    Delete 09-09-2026/20-09-2026 directory

--- 09-09-2026/20-09-2026/Decimal_to_Binary.java
+++
@@ -1,7 +0,0 @@
-public class Decimal_to_Binary {
-    public static void main(String[] args) {
-        int decimal = 10;
-        String binary = Integer.toBinaryString(decimal);
-        System.out.println("Binary of " + decimal + " is: " + binary);
-    }
-}
